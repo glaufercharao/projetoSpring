c0b0d20cd7451b5c328d216a590ec14d72fff198
Gláufer Charão <glaufer.charao@gmail.com>
2019-05-01 20:10:14 -0300

    Criado classe Cliente, TipoCliente, Endereços e um tipo Set para Telefones

--- src/main/java/com/projetospring/repositores/ClienteRepository.java
+++ src/main/java/com/projetospring/repositores/ClienteRepository.java
@@ -2,9 +2,9 @@ package com.projetospring.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.projetospring.dominio.Categoria;
+import com.projetospring.dominio.Cliente;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
--- src/main/java/com/projetospring/repositores/EnderecoRepository.java
+++ src/main/java/com/projetospring/repositores/EnderecoRepository.java
@@ -2,9 +2,9 @@ package com.projetospring.repositores;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.projetospring.dominio.Categoria;
+import com.projetospring.dominio.Endereco;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
 
 }
